82bc84c0790cb355860380e2b4ef1c56728a4e7c
Dinithi Nanayakkara <dinijayo@gmail.com>
2025-08-24 23:27:38 +0530

    feat: frontend

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,12 +9,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
+  return <main className="bg-[url('/images/bg-main.svg')]bg-cover">
+    <Navbar/>
       <section className="main-section">
         <div className={"page-heading"}>
         <h1>Track your Applications and Resume Ratings</h1>
           <h2>Review your submissions and check AI powered feedback. </h2>
         </div>
       </section>
+
   </main>;
 }
