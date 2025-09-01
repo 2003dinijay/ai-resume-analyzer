6fb804ced4aa66e4b94351a94ed319169e4d271f
Dinithi Nanayakkara <dinijayo@gmail.com>
2025-09-01 20:34:45 +0530

    feat: frontend

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,22 +1,35 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import { resumes } from '../../constants';
+import ResumeCard from '~/components/ResumeCard';
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Track applications and AI feedback for your resumes" },
   ];
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')]bg-cover">
-    <Navbar/>
-      <section className="main-section">
-        <div className={"page-heading"}>
-        <h1>Track your Applications and Resume Ratings</h1>
-          <h2>Review your submissions and check AI powered feedback. </h2>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen">
+      <Navbar />
+      <section className="main-section overflow-x-hidden">
+        <div className="page-heading py-16" >
+          <h1>Track your Applications and Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
         </div>
-      </section>
 
-  </main>;
+        {resumes.length > 0 ? (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-gray-500 mt-10 text-center">No resumes submitted yet.</p>
+        )}
+      </section>
+    </main>
+  );
 }
